33133f9f83b5ba4cf8b5d8cd77209e5dce23c9cb
Ctlna <150844717+Ctlna@users.noreply.github.com>
2024-01-30 06:39:29 -0500

    ejercicio_MulfordCatalina.py

--- Dia3/ejercicio_MulfordCatalina.py
+++ Dia3/ejercicio_MulfordCatalina.py
@@ -3,10 +3,9 @@ ten = []
 five = []
 one = []
 def cn(ca, n, cash):
-    cu = ca // n 
-    cash.append(cu)
-    return ca - cu * n
+    cash.append(ca // n)
+    return ca - (ca // n) * n
 m = cn(m, 10, ten)
 m = cn(m, 5, five)
 m = cn(m, 1, one)
-print(ten[0], " coins of ten, ", five[0], " coins of five, ", one[0], " coins of one.")
+print(ten[0],  five[0],  one[0])
