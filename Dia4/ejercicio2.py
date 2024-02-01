8e2b1128886e45070ebfd008f64113df99f215ea
Ctlna <150844717+Ctlna@users.noreply.github.com>
2024-01-31 19:50:25 -0500

    ejercicio2.py

--- Dia4/ejercicio2.py
+++ Dia4/ejercicio2.py
@@ -1,43 +1,51 @@
-def C(x,coins):
+def C(x, ye):
     for i in range(x):
-        n=int(input())
-        if 4<=n<=50:
-            coins.append[n]
-        else:
-            exit()
-def T(x,table):
-    for i in range(1,x):
-        n=int(input())
-        if 1<=n<=10:
-            table.append[n]
-        else:
-            exit()
+        n = int(input())
+        ye.append(n)
+
+def T(x, ye):
+    for i in range(x):
+        n = int(input())
+        ye.append(n)
+
 def calcu(lc, lt, List_coins):
     result = []
 
     for i in range(lc):
-        set_list = List_coins[i * lt : (i + 1) * lt]
+        set_list = List_coins[i * lt: (i + 1) * lt]
         length = sum(set_list[:lt // 2]) - sum(set_list[lt // 2:])
         gleg = max(x for x in set_list if x <= length)
         smalleg = min(x for x in set_list if x >= length)
         result.append((gleg, smalleg))
     return result
 
-cont=True
+cont = True
 
 while cont:
-    coins=()
-    table=[]
-    cosito=input()
+    coins = []
+    table = []
+    cosito = input()
     cc, ct = map(int, cosito.split())
-    if  cc==0 and ct==0:
-        cont=False
+
+    if cc == 0 and ct == 0:
+        cont = False
     else:
-       C(cc,coins)
-       T(ct,table)
+        C(cc, coins)
+        T(ct, table)
+
+        if 4 <= len(coins) <= 50:
+            pass  
+        else:
+            exit("")
+
+        if 1 <= len(table) <= 10:
+            pass 
+        else:
+            exit("")
+
     output = calcu(cc, ct, coins)
     for leles in output:
-     print(f"{leles[0]} {leles[1]}")
-   
+        print(f"{leles[0]} {leles[1]}")
+
 #Catalina Mulford Monroy
-#Daniela Forero 
+#Daniela Forero Ballen
