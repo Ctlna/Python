ee9de4867113ae4d4d2eee9bd02e94dc4f5f06c3
Ctlna <150844717+Ctlna@users.noreply.github.com>
2024-02-02 13:34:48 -0500

    Add files via upload

---
+++ Dia7/dia3/Modulosd3.py
@@ -0,0 +1,9 @@
+ten = []
+five = []
+one = []
+
+def cn(ca, n, cash):
+    cash.append(ca // n)
+    return ca - (ca // n) * n
+
+##Catalina Mulford Monroy P1
---
+++ Dia7/dia3/ejercicio.py
@@ -0,0 +1,10 @@
+##Catalina Mulford Monroy P1
+import Modulosd3
+m = int(input())
+
+
+m = Modulosd3.cn(m, 10, Modulosd3.ten)
+m = Modulosd3.cn(m, 5, Modulosd3.five)
+m = Modulosd3.cn(m, 1, Modulosd3.one)
+
+print(Modulosd3.ten[0],  Modulosd3.five[0],  Modulosd3.one[0])
