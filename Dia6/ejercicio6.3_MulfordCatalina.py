4300a68e2cc1f90431a9ff25dad67f4474904922
Ctlna <150844717+Ctlna@users.noreply.github.com>
2024-02-01 20:26:11 -0500

    Add files via upload

---
+++ Dia6/ejercicio6.3_MulfordCatalina.py
@@ -0,0 +1,21 @@
+def ball_collide(l,a,x):
+    sumr=l+a
+    if x<=sumr:
+        print(True)
+    else:
+        print(False)
+
+#cosito1
+xbu=int(input())
+ybu=int(input())
+r=int(input())
+rau=2*r
+
+#cosito2
+xpt=int(input())
+ypt=int(input())
+dio=int(input())
+rtw=2*dio
+
+di=((xpt-xbu)**2 + (ypt-ybu)**2)**0.5
+ball_collide(rau,rtw,di)
