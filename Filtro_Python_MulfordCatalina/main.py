1ea1c3e3d85184cc2286e14a31d99e9fc94e652b
Ctlna <150844717+Ctlna@users.noreply.github.com>
2024-02-16 06:51:22 -0500

    Add files via upload

---
+++ Filtro_Python_MulfordCatalina/main.py
@@ -0,0 +1,9 @@
+#Catalina Mulford
+import json
+file=("data.json","r")
+datas=json.load 
+
+qHacer=int(input("Bienvenido, que deseas hacer:\n 1. Perfiles Usuario\n 2. Historial usuarios\n 3. Personalización de Servicios\n 4. Gestión de Fidelización"))
+
+if qHacer==1:
+    
